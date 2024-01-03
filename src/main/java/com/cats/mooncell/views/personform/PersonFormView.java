45d89a7fe1be1f464979bef224ccc3b2b10b8793
mehulklh <106767917+mehulklh@users.noreply.github.com>
2024-01-03 15:30:05 +0530

    person form parital

--- src/main/java/com/cats/mooncell/views/personform/PersonFormView.java
+++ src/main/java/com/cats/mooncell/views/personform/PersonFormView.java
@@ -4,27 +4,85 @@ import com.cats.mooncell.views.MainLayout;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import jakarta.annotation.security.PermitAll;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @PageTitle("Person Form")
 @Route(value = "person-form", layout = MainLayout.class)
 @PermitAll
 @Uses(Icon.class)
 public class PersonFormView extends Composite<VerticalLayout> {
+    private static final Set<String> states = new LinkedHashSet<>();
+    private static final Set<String> countries = new LinkedHashSet<>();
+
+    static {
+        states.addAll(Arrays.asList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+                "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+                "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+                "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+                "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+                "West Virginia", "Wisconsin", "Wyoming"));
+
+        countries.addAll(Arrays.asList("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola",
+                "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia",
+                "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize",
+                "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island",
+                "Brazil", "British Indian Ocean Territory", "British Virgin Islands", "Brunei Darussalam", "Bulgaria",
+                "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
+                "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands",
+                "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus",
+                "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador",
+                "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands",
+                "Faroe Islands", "Federated States of Micronesia", "Fiji", "Finland", "France", "French Guiana",
+                "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
+                "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea",
+                "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong",
+                "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast",
+                "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
+                "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau",
+                "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
+                "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Moldova", "Monaco", "Mongolia",
+                "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands",
+                "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue",
+                "Norfolk Island", "North Korea", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau",
+                "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal",
+                "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis",
+                "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
+                "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia",
+                "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands",
+                "South Korea", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname",
+                "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic",
+                "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago",
+                "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine",
+                "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands",
+                "United States Virgin Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela",
+                "Vietnam", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia",
+                "Zimbabwe"));
+    }
 
     public PersonFormView() {
         VerticalLayout layoutColumn2 = new VerticalLayout();
@@ -63,17 +121,105 @@ public class PersonFormView extends Composite<VerticalLayout> {
         buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonSecondary.setText("Cancel");
         buttonSecondary.setWidth("min-content");
-        getContent().add(layoutColumn2);
-        layoutColumn2.add(h3);
-        layoutColumn2.add(formLayout2Col);
-        formLayout2Col.add(textField);
-        formLayout2Col.add(textField2);
-        formLayout2Col.add(datePicker);
-        formLayout2Col.add(textField3);
-        formLayout2Col.add(emailField);
-        formLayout2Col.add(textField4);
-        layoutColumn2.add(layoutRow);
-        layoutRow.add(buttonPrimary);
-        layoutRow.add(buttonSecondary);
+//        getContent().add(layoutColumn2);
+//        layoutColumn2.add(h3);
+//        layoutColumn2.add(formLayout2Col);
+//        formLayout2Col.add(textField);
+//        formLayout2Col.add(textField2);
+//        formLayout2Col.add(datePicker);
+//        formLayout2Col.add(textField3);
+//        formLayout2Col.add(emailField);
+//        formLayout2Col.add(textField4);
+//        layoutColumn2.add(layoutRow);
+//        layoutRow.add(buttonPrimary);
+//        layoutRow.add(buttonSecondary);
+        getContent().add(createPersonalDetailsSection());
+        getContent().add(createShippingAddressSection());
+    }
+
+    private Section createPersonalDetailsSection() {
+        Section personalDetails = new Section();
+        personalDetails.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
+                LumoUtility.Margin.Bottom.XLARGE, LumoUtility.Margin.Top.MEDIUM);
+
+       /* Paragraph stepOne = new Paragraph("Checkout 1/3");
+        stepOne.addClassNames(LumoUtility.Margin.NONE, LumoUtility.FontSize.SMALL, LumoUtility.TextColor.SECONDARY);*/
+
+        H3 header = new H3("Personal details");
+        header.addClassNames(LumoUtility.Margin.Bottom.MEDIUM, LumoUtility.Margin.Top.SMALL,
+                LumoUtility.FontSize.XXLARGE, LumoUtility.AlignContent.START);
+
+        TextField name = new TextField("Name");
+        name.setRequiredIndicatorVisible(true);
+        name.setPattern("[\\p{L} \\-]+");
+        name.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        EmailField email = new EmailField("Email address");
+        email.setRequiredIndicatorVisible(true);
+        email.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        TextField phone = new TextField("Phone number");
+        phone.setRequiredIndicatorVisible(true);
+        phone.setPattern("[\\d \\-\\+]+");
+        phone.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        Checkbox rememberDetails = new Checkbox("Remember personal details for next time");
+        rememberDetails.addClassNames(LumoUtility.Margin.Top.SMALL);
+
+        personalDetails.add(header, name, email, phone, rememberDetails);
+        return personalDetails;
+    }
+
+    private Section createShippingAddressSection() {
+        Section shippingDetails = new Section();
+        shippingDetails.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN, LumoUtility.Margin.Bottom.XLARGE, LumoUtility.Margin.Top.MEDIUM);
+
+//        Paragraph stepTwo = new Paragraph("Checkout 2/3");
+//        stepTwo.addClassNames(LumoUtility.Margin.NONE, LumoUtility.FontSize.SMALL, LumoUtility.TextColor.SECONDARY);
+
+        H3 header = new H3("Address");
+        header.addClassNames(LumoUtility.Margin.Bottom.MEDIUM, LumoUtility.Margin.Top.SMALL,
+                LumoUtility.FontSize.XXLARGE,LumoUtility.AlignContent.START);
+
+        ComboBox<String> countrySelect = new ComboBox<>("Country");
+        countrySelect.setRequiredIndicatorVisible(true);
+        countrySelect.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        TextArea address = new TextArea("Street address");
+        address.setMaxLength(200);
+        address.setRequiredIndicatorVisible(true);
+        address.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        Div subSection = new Div();
+        subSection.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexWrap.WRAP, Gap.MEDIUM);
+
+        TextField postalCode = new TextField("Postal Code");
+        postalCode.setRequiredIndicatorVisible(true);
+        postalCode.setPattern("[\\d \\p{L}]*");
+        postalCode.addClassNames(LumoUtility.Margin.Bottom.SMALL);
+
+        TextField city = new TextField("City");
+        city.setRequiredIndicatorVisible(true);
+        city.addClassNames(LumoUtility.Flex.GROW, LumoUtility.Margin.Bottom.SMALL);
+
+        subSection.add(postalCode, city);
+
+        ComboBox<String> stateSelect = new ComboBox<>("State");
+        stateSelect.setRequiredIndicatorVisible(true);
+
+        stateSelect.setItems(states);
+        stateSelect.setVisible(false);
+        countrySelect.setItems(countries);
+        countrySelect
+                .addValueChangeListener(e -> stateSelect.setVisible(countrySelect.getValue().equals("United States")));
+
+        Checkbox sameAddress = new Checkbox("Billing address is the same as shipping address");
+        sameAddress.addClassNames(LumoUtility.Margin.Top.SMALL);
+
+        Checkbox rememberAddress = new Checkbox("Remember address for next time");
+
+        shippingDetails.add(header, countrySelect, address, subSection, stateSelect, sameAddress,
+                rememberAddress);
+        return shippingDetails;
     }
 }
